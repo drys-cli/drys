27e610b6bd6a0af6652efb29fbaaa491b0e7beb2
Haris Gušić <harisgusic.dev@gmail.com>
2022-07-30 14:40:56 +0200

    Update pipenv env

--- .tem/env/@fish/python.fish
+++ .tem/env/@fish/python.fish
@@ -1,11 +1,3 @@
-# Produce the same effects in the environment as 'pipenv run' would
-set -e PIPENV_ACTIVE
-set -l _path (pipenv run env | grep "^PATH=" | sed 's/^PATH=//')
-set -l _virtualenv (pipenv run env | grep "^VIRTUAL_ENV=" | sed 's/^VIRTUAL_ENV=//')
-export PATH="$_path"
-export VIRTUAL_ENV="$_virtualenv"
-set -gx PIPENV_ACTIVE 1
-
 if [ -z "$__PYTHONSTARTUP_DEFAULT" ]
     set -gx __PYTHONSTARTUP_DEFAULT "$PYTHONSTARTUP"
 end
--- .tem/vars.py
+++ .tem/vars.py
@@ -2,7 +2,7 @@ from tem.var import Variable, Variant
 
 # TODO implement this functionality
 
-use_pipenv = Variant(True)
+use_pipenv = Variant(False)
 env = Variable(["prod", "dev"], default="dev")
 test1 = Variable(str, default="n")
 anyv = Variable()
